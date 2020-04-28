b03069e56e9be143c590e7c00d758faac1c5f098
Archit Khode <arkits@outlook.com>
2020-04-27 21:40:51 -0700

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,12 @@
-const decodeAuthHeader = (authHeader) => {
-    // parse login and password from headers
-    let b64auth = (authHeader || '').split(' ')[1] || '';
-    let decodedAuthHeader = Buffer.from(b64auth, 'base64').toString().split(':');
-    return decodedAuthHeader;
-};
+/**
+ * Decodes the HTTP Authorization header
+ * @param authHeader - Authorization header as a String
+ * @returns [username, password]
+ */
+function decodeAuthHeader(authHeader: string): string[] {
+  let b64auth = (authHeader || "").split(" ")[1] || "";
+  let decodedAuthHeader = Buffer.from(b64auth, "base64").toString().split(":");
+  return decodedAuthHeader;
+}
 
 export { decodeAuthHeader };
