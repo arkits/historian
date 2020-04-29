92cb38ca93a01690c4aa43bf2d5d5347d0f75a26
Archit Khode <arkits@outlook.com>
2020-04-28 22:03:06 -0700

    added logic for delete and update user

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,16 +1,15 @@
 import { Request, Response } from 'express';
-import * as bunyan from 'bunyan';
 import { User } from '../entity/User';
-import { getRepository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import * as config from 'config';
+import { saveUser, modifyUser, getUserByUsername, deleteUserByUsername } from '../dao/UserDao';
+import { logger } from '../domain/Logger';
+import { decodeAuthHeader } from '../utils';
 
-const logger = bunyan.createLogger({ name: 'historian' });
-
-const registerUser = async (request: Request, response: Response) => {
+async function registerUser(request: Request, response: Response) {
     let body = request.body;
 
-    logger.info('In registerUser - ', body);
+    logger.info('[register-user] got new user request - ', body);
 
     let user = new User();
 
@@ -24,16 +23,18 @@ const registerUser = async (request: Request, response: Response) => {
 
     if (body.password) {
         let hashedPassword = await bcrypt.hash(body.password, config.get('auth.saltRounds'));
-        logger.info('[register-user] generated hased password - ', hashedPassword);
         user.password = hashedPassword;
     }
 
-    let result = await addUser(user);
+    let savedUser = await saveUser(user);
+    logger.info('[register-user] saved user to db! - ', savedUser);
 
-    if (result) {
+    if (savedUser !== null) {
+        delete savedUser.password;
         response.status(200);
         response.json({
-            message: 'Registered User!'
+            message: 'Registered User!',
+            user: savedUser
         });
         return;
     } else {
@@ -44,21 +45,77 @@ const registerUser = async (request: Request, response: Response) => {
         });
         return;
     }
-};
+}
 
-const addUser = async (user: User) => {
-    var result = false;
-    var userRepository = getRepository(User);
+async function deleteUser(request: Request, response: Response) {
+    // extract username from request headers
+    let [username, _] = decodeAuthHeader(request.headers.authorization);
 
-    try {
-        await userRepository.save(user);
-        result = true;
-    } catch (error) {
-        result = false;
-        logger.error(error);
+    let result = await deleteUserByUsername(username);
+
+    if (result.affected === 1) {
+        response.status(200);
+        response.json({
+            message: 'Deleted User!'
+        });
+        return;
+    } else {
+        response.status(400);
+        response.json({
+            error: 'Failed to Update User!',
+            error_description: 'Failed to Update User!'
+        });
+        return;
+    }
+}
+
+async function updateUser(request: Request, response: Response) {
+    // extract username from request headers
+    let [username, _] = decodeAuthHeader(request.headers.authorization);
+
+    let user = await getUserByUsername(username);
+
+    let body = request.body;
+
+    // Failes due to foreign key
+    // if (body.username) {
+    //     user.username = body.username;
+    // }
+
+    if (body.name) {
+        user.name = body.name;
     }
 
-    return result;
-};
+    if (body.password) {
+        let hashedPassword = await bcrypt.hash(body.password, config.get('auth.saltRounds'));
+        user.password = hashedPassword;
+    }
 
-export { registerUser };
+    let updateUserResult = await modifyUser(user);
+
+    if (updateUserResult.affected === 1) {
+        // get updated user from db
+        let updatedUser = await getUserByUsername(username);
+        logger.info('[update-user] user got updated to - ', updatedUser);
+
+        // remove password
+        delete updatedUser.password;
+
+        // complete the request
+        response.status(200);
+        response.json({
+            message: 'Updated User!',
+            user: updatedUser
+        });
+        return;
+    } else {
+        response.status(400);
+        response.json({
+            error: 'Failed to Update User!',
+            error_description: 'Failed to Update User!'
+        });
+        return;
+    }
+}
+
+export { registerUser, deleteUser, updateUser };
--- src/dao/UserDao.ts
+++ src/dao/UserDao.ts
@@ -1,15 +1,53 @@
 import { User } from '../entity/User';
-import * as bunyan from 'bunyan';
-import { getRepository } from 'typeorm';
+import { getRepository, DeleteResult, UpdateResult } from 'typeorm';
+import { logger } from '../domain/Logger';
 
-const logger = bunyan.createLogger({ name: 'historian' });
+async function saveUser(user: User): Promise<User> {
+    let savedUser = null;
+    let userRepository = getRepository(User);
+    try {
+        savedUser = await userRepository.save(user);
+    } catch (error) {
+        logger.error(error);
+    }
+    return savedUser;
+}
 
-const getUserByUsername = async (username) => {
-    var userRepository = getRepository(User);
+async function getUserByUsername(username: string): Promise<User> {
+    let userRepository = getRepository(User);
     let user = await userRepository.findOne({
         username: username
     });
     return user;
-};
+}
 
-export { getUserByUsername };
+async function deleteUserByUsername(username: string): Promise<DeleteResult> {
+    let userRepository = getRepository(User);
+    let result = null;
+    try {
+        result = await userRepository.delete({
+            username: username
+        });
+    } catch (error) {
+        logger.error(error);
+    }
+    return result;
+}
+
+async function modifyUser(user: User): Promise<UpdateResult> {
+    let userRepository = getRepository(User);
+    let result = null;
+    try {
+        result = await userRepository.update(
+            {
+                id: user.id
+            },
+            user
+        );
+    } catch (error) {
+        logger.error(error);
+    }
+    return result;
+}
+
+export { getUserByUsername, saveUser, deleteUserByUsername, modifyUser };
