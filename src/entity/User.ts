964a88e96f740a2118f1fa8ea70691a1cbfba93d
Archit Khode <arkits@outlook.com>
2020-04-28 22:04:05 -0700

    patch user entity to cast id to string

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class User {
     @PrimaryGeneratedColumn('uuid')
-    id: number;
+    id: string;
 
     @Column()
     name: string;
