5b5a2e7f7308ecb1d2c792e9510dc2c817e9d247
Archit Khode <arkits@outlook.com>
2020-05-09 21:35:47 -0700

    [backend] impl serving frontend via backend

--- src/backend/src/index.ts
+++ src/backend/src/index.ts
@@ -5,12 +5,20 @@ import * as bodyParser from 'body-parser';
 import { router } from './routes';
 import { logger } from './domain/Logger';
 import * as config from 'config';
+import * as path from 'path';
 
 // create express app
 var app = express();
 
 // add those middlewares
 app.use(bodyParser.json());
+
+// Serve the static files from the React app
+if (config.get('frontend.serve')) {
+    let completeFrontendPath = path.join(__dirname, config.get('frontend.location'));
+    logger.info('Serving frontend from %s', completeFrontendPath);
+    app.use('/historian', express.static(completeFrontendPath));
+}
 
 // add them routes
 app.use('/', router);
