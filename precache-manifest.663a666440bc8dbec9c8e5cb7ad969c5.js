cfcbe3969cf1e22af27102bc3e55cbc2e97bee7c
Archit Khode <arkits@outlook.com>
2020-05-09 21:33:26 -0700

    Updates

---
+++ precache-manifest.663a666440bc8dbec9c8e5cb7ad969c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a98b900084846ea250488459869155",
+    "url": "/historian/index.html"
+  },
+  {
+    "revision": "62dc9e6ba308944f2a67",
+    "url": "/historian/static/css/main.25420842.chunk.css"
+  },
+  {
+    "revision": "6177d245cfeea351a1b2",
+    "url": "/historian/static/js/2.84168cc5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/historian/static/js/2.84168cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62dc9e6ba308944f2a67",
+    "url": "/historian/static/js/main.94f2890d.chunk.js"
+  },
+  {
+    "revision": "456f4539dea1acb868ea",
+    "url": "/historian/static/js/runtime-main.60401063.js"
+  }
+]);
