8eab9a84aa1bca2aaf36b605e66d898a9d5831d2
Archit Khode <arkits@outlook.com>
2020-05-10 17:58:04 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/historian/precache-manifest.0fcee9d6f72ad96d8ec5a0727d817e55.js"
+  "/historian/precache-manifest.de7e5ac2c823fd533849629dce3104ca.js"
 );
 
 self.addEventListener('message', (event) => {
