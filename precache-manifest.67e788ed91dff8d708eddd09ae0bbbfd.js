59c9ae72acc6308451a3bd1a46c51add40220304
Archit Khode <arkits@outlook.com>
2020-05-16 00:19:10 -0700

    Updates

---
+++ precache-manifest.67e788ed91dff8d708eddd09ae0bbbfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca2ea92d72b026b4d72745583a42bba",
+    "url": "/historian/index.html"
+  },
+  {
+    "revision": "0748ffbfa11e1fd324ea",
+    "url": "/historian/static/css/main.4859058b.chunk.css"
+  },
+  {
+    "revision": "299f2b1776bab4c375ec",
+    "url": "/historian/static/js/2.aebefd8b.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/historian/static/js/2.aebefd8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0748ffbfa11e1fd324ea",
+    "url": "/historian/static/js/main.c4bdecac.chunk.js"
+  },
+  {
+    "revision": "38196aaa76b058187acc",
+    "url": "/historian/static/js/runtime-main.1472b55d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/historian/precache-manifest.6df12b9e83ea306ab979b5e21465b1ac.js"
+  "/historian/precache-manifest.67e788ed91dff8d708eddd09ae0bbbfd.js"
 );
 
 self.addEventListener('message', (event) => {
