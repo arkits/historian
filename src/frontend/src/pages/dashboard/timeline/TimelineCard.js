4d2bfd704b715f637a42548f8e5deec2f96c8a44
Archit Khode <arkits@outlook.com>
2020-05-24 16:09:05 -0700

    [ui] add compat for lastfm_nowplaying

--- src/frontend/src/pages/dashboard/timeline/TimelineCard.js
+++ src/frontend/src/pages/dashboard/timeline/TimelineCard.js
@@ -5,6 +5,7 @@ import { red } from '@material-ui/core/colors';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import RedditIcon from '@material-ui/icons/Reddit';
 import DescriptionIcon from '@material-ui/icons/Description';
+import MusicNoteRoundedIcon from '@material-ui/icons/MusicNoteRounded';
 import moment from 'moment';
 
 const useStyles = makeStyles((theme) => ({
@@ -31,12 +32,16 @@ function TimelineCard(props) {
         let title = '';
         if (history?.type === 'instagram_saved') {
             title = history?.metadata?.caption;
+        } else if (history?.type === 'lastfm_nowplaying') {
+            title = history?.metadata?.name;
         } else {
             title = history?.metadata?.title;
         }
 
         try {
-            title = title.slice(0, 150);
+            if (title.length > 150) {
+                title = title.slice(0, 150) + '...';
+            }
         } catch (error) {
             title = '[No Title]';
         }
@@ -49,6 +54,8 @@ function TimelineCard(props) {
             return <InstagramIcon />;
         } else if (history?.type === 'reddit_saved') {
             return <RedditIcon />;
+        } else if (history?.type === 'lastfm_nowplaying') {
+            return <MusicNoteRoundedIcon />;
         } else {
             return <DescriptionIcon />;
         }
@@ -57,6 +64,8 @@ function TimelineCard(props) {
     const getPrettyUsername = () => {
         if (history?.type === 'instagram_saved') {
             return history?.metadata?.username;
+        } else if (history?.type === 'lastfm_nowplaying') {
+            return history?.metadata?.artist['#text'] + ' // ' + history?.metadata?.album['#text'];
         } else {
             return history?.metadata?.author;
         }
@@ -65,6 +74,8 @@ function TimelineCard(props) {
     const getThumbnail = () => {
         if (history?.type === 'instagram_saved') {
             return history?.metadata?.mediaUrls[0];
+        } else if (history?.type === 'lastfm_nowplaying') {
+            return history?.metadata?.image[history?.metadata?.image.length - 1]['#text'];
         } else {
             return history?.metadata?.thumbnail;
         }
@@ -73,6 +84,8 @@ function TimelineCard(props) {
     const getPermalink = () => {
         if (history?.type === 'instagram_saved') {
             return history?.metadata?.mediaUrls[0];
+        } else if (history?.type === 'lastfm_nowplaying') {
+            return history?.metadata?.url;
         } else {
             return history?.metadata?.content_url;
         }
