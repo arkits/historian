2a82ed357ac9049d428eb3919549995153f20328
Archit Khode <arkits@outlook.com>
2020-07-06 20:46:18 -0700

    [ui] Refactor TimelineCard and GalleryCard

--- src/frontend/src/pages/dashboard/timeline/TimelineCard.js
+++ src/frontend/src/pages/dashboard/timeline/TimelineCard.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import { Typography, Card, CardContent, Grid, CardHeader, Avatar, CardActionArea } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Card, CardContent, Grid, CardHeader, Avatar, CardActionArea } from '@material-ui/core';
+
+import moment from 'moment';
+
+import TimelineCardDialog from './TimelineCardDialog';
+import * as HistoryParser from '../api/HistoryParser';
+
 import { red } from '@material-ui/core/colors';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import RedditIcon from '@material-ui/icons/Reddit';
 import DescriptionIcon from '@material-ui/icons/Description';
 import MusicNoteRoundedIcon from '@material-ui/icons/MusicNoteRounded';
-import moment from 'moment';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Button from '@material-ui/core/Button';
-import { useTheme } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
     card: {
@@ -33,76 +28,21 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box
-                    p={3}
-                    style={{
-                        borderStyle: 'hidden solid solid',
-                        borderRadius: '0px 0px 10px 10px',
-                        borderWidth: '1px'
-                    }}
-                >
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
 function TimelineCard(props) {
     const classes = useStyles();
 
     const [infoDialogOpen, setInfoDialogOpen] = React.useState(false);
     const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
-    const handleClickOpen = () => {
+    const handleDialogOpen = () => {
         setInfoDialogOpen(true);
     };
 
-    const handleClose = () => {
+    const handleDialogClose = () => {
         setInfoDialogOpen(false);
     };
 
-    const [value, setValue] = React.useState(0);
-
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
     let history = props.history;
-
-    const getPrettyTitle = () => {
-        let title = '';
-        if (history?.type === 'instagram_saved') {
-            title = history?.metadata?.caption;
-        } else if (history?.type === 'lastfm_nowplaying') {
-            title = history?.metadata?.name;
-        } else {
-            title = history?.metadata?.title;
-        }
-
-        try {
-            if (title.length > 150) {
-                title = title.slice(0, 150) + '...';
-            }
-        } catch (error) {
-            title = '[No Title]';
-        }
-
-        return title;
-    };
 
     const getPrettyAvatar = () => {
         if (history?.type === 'instagram_saved') {
@@ -116,48 +56,19 @@ function TimelineCard(props) {
         }
     };
 
-    const getPrettyUsername = () => {
-        if (history?.type === 'instagram_saved') {
-            return history?.metadata?.username;
-        } else if (history?.type === 'lastfm_nowplaying') {
-            return history?.metadata?.artist['#text'] + ' // ' + history?.metadata?.album['#text'];
-        } else {
-            return history?.metadata?.author;
-        }
-    };
-
-    const getThumbnail = () => {
-        if (history?.type === 'instagram_saved') {
-            return history?.metadata?.mediaUrls[0];
-        } else if (history?.type === 'lastfm_nowplaying') {
-            return history?.metadata?.image[history?.metadata?.image.length - 1]['#text'];
-        } else {
-            return history?.metadata?.thumbnail;
-        }
-    };
-
-    const getPermalink = () => {
-        if (history?.type === 'instagram_saved') {
-            return history?.metadata?.mediaUrls[0];
-        } else if (history?.type === 'lastfm_nowplaying') {
-            return history?.metadata?.url;
-        } else {
-            return history?.metadata?.content_url;
-        }
-    };
     return (
         <div>
             <Card className={classes.card}>
                 <CardActionArea>
                     <Grid container spacing={0}>
                         <Grid item xs={9}>
-                            <CardContent className={classes.content} onClick={handleClickOpen}>
+                            <CardContent className={classes.content} onClick={handleDialogOpen}>
                                 <Typography variant="body1" color="textPrimary" component="p">
-                                    {getPrettyTitle()}
+                                    {HistoryParser.getPrettyTitle(history)}
                                 </Typography>
                                 <CardHeader
                                     avatar={<Avatar className={classes.avatar}>{getPrettyAvatar()}</Avatar>}
-                                    title={getPrettyUsername()}
+                                    title={HistoryParser.getPrettyUsername(history)}
                                     subheader={moment(history?.timestamp).fromNow()}
                                     style={{
                                         paddingLeft: '0',
@@ -166,61 +77,20 @@ function TimelineCard(props) {
                                 />
                             </CardContent>
                         </Grid>
-                        <Grid align="right" item xs={3} onClick={() => window.open(getPermalink(), '_blank')}>
-                            <Avatar variant="square" className={classes.contentPreview} src={getThumbnail()}></Avatar>
+                        <Grid align="right" item xs={3}>
+                            <a href={HistoryParser.getPermalink(history)}>
+                                <Avatar
+                                    variant="square"
+                                    className={classes.contentPreview}
+                                    src={HistoryParser.getThumbnail(history)}
+                                ></Avatar>
+                            </a>
                         </Grid>
                     </Grid>
                 </CardActionArea>
             </Card>
 
-            <Dialog
-                fullScreen={fullScreen}
-                open={infoDialogOpen}
-                onClose={handleClose}
-                aria-labelledby="responsive-dialog-title"
-                maxWidth={'md'}
-                fullWidth
-            >
-                <DialogTitle id="responsive-dialog-title">{getPrettyTitle()}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        <Tabs
-                            value={value}
-                            onChange={handleChange}
-                            indicatorColor="primary"
-                            textColor="primary"
-                            centered
-                            style={{
-                                backgroundColor: '#212121'
-                            }}
-                        >
-                            <Tab label="About" />
-                            <Tab label="Raw" />
-                        </Tabs>
-
-                        <TabPanel value={value} index={0}>
-                            <img src={getThumbnail()} alt={getPrettyTitle()} />
-                        </TabPanel>
-                        <TabPanel value={value} index={1}>
-                            <pre
-                                style={{
-                                    fontFamily: 'monospace'
-                                }}
-                            >
-                                {JSON.stringify(history, null, 4)}
-                            </pre>
-                        </TabPanel>
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button autoFocus href={getPermalink()} target="_blank" color="primary">
-                        Permalink
-                    </Button>
-                    <Button onClick={handleClose} color="primary" autoFocus>
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>
+            <TimelineCardDialog history={history} handleDialogClose={handleDialogClose} open={infoDialogOpen} />
 
             <br />
         </div>
---
+++ src/frontend/src/pages/dashboard/timeline/TimelineCardDialog.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button, Tabs, Tab, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import * as HistoryParser from '../api/HistoryParser';
+
+const useStyles = makeStyles((theme) => ({
+    card: {
+        display: 'flex'
+    },
+    contentPreview: {
+        width: theme.spacing(18),
+        height: theme.spacing(18)
+    }
+}));
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box
+                    p={3}
+                    style={{
+                        backgroundColor: '#263238'
+                    }}
+                >
+                    <>{children}</>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function TimelineCardDialog(props) {
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    let history = props.history;
+    let handleDialogClose = props.handleDialogClose;
+    let open = props.open;
+
+    const [value, setValue] = React.useState(0);
+
+    const handleTabChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div>
+            <Dialog
+                fullScreen={fullScreen}
+                open={open}
+                onClose={handleDialogClose}
+                aria-labelledby="responsive-dialog-title"
+                maxWidth={'md'}
+                fullWidth
+            >
+                <DialogTitle id="responsive-dialog-title">{HistoryParser.getPrettyTitle(history)}</DialogTitle>
+                <DialogContent>
+                    <Tabs
+                        value={value}
+                        onChange={handleTabChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        centered
+                        style={{
+                            backgroundColor: '#212121'
+                        }}
+                    >
+                        <Tab label="About" />
+                        <Tab label="Raw" />
+                    </Tabs>
+
+                    <TabPanel value={value} index={0}>
+                        <img src={HistoryParser.getThumbnail(history)} alt={HistoryParser.getPrettyTitle(history)} />
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        <pre
+                            style={{
+                                fontFamily: 'monospace'
+                            }}
+                        >
+                            {JSON.stringify(history, null, 4)}
+                        </pre>
+                    </TabPanel>
+                </DialogContent>
+                <DialogActions>
+                    <Button autoFocus href={HistoryParser.getPermalink(history)} target="_blank" color="primary">
+                        Permalink
+                    </Button>
+                    <Button onClick={handleDialogClose} color="primary" autoFocus>
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default TimelineCardDialog;
