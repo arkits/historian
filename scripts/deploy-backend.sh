46abfd1486fd0f3b49ebec477a7ef0364fb86970
Archit Khode <arkits@outlook.com>
2022-05-25 20:31:45 -0700

    ci: impl ci

---
+++ scripts/deploy-backend.sh
@@ -0,0 +1,15 @@
+set -e
+
+echo ">>> inside deploy-backend.sh"
+
+cd /opt/software/historian/
+
+echo ">>> pulling latest"
+git pull
+
+echo ">>> installing dependencies"
+npm install
+
+pm2 restart "historian-backend"
+
+# pm2 start --name "historian-backend" npm -- start
