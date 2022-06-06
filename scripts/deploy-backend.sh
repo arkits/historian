46ae39530f4345554181dda49e9aac3a9f3debed
Archit Khode <arkits@outlook.com>
2022-06-05 18:59:32 -0700

    ci: add prisma related

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -10,6 +10,10 @@ git pull
 echo ">>> installing dependencies"
 npm install
 
+echo ">>> prisma migrations"
+npx prisma generate
+# npx prisma migrate deploy
+
 pm2 restart "historian-backend"
 
-# pm2 start --name "historian-backend" npm -- run serve:backend 
+# pm2 start --name "historian-backend" npm -- run serve:backend
