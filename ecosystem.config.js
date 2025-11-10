534d96b73b6972d61b795e5240b3b18de465beab
Archit Khode <arkits@outlook.com>
2025-11-10 00:32:27 -0800

    feat: add metadata column to Account table and update environment variables in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
     args: 'run backend:run:prod',
     cwd: '/opt/software/historian',
     env: {
-      NODE_ENV: 'production'
+      NODE_ENV: 'production',
+      BETTER_AUTH_URL: 'https://historian-api.archit.xyz'
     },
     instances: 1,
     autorestart: true,
