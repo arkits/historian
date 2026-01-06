ea668095ea1d41854f5d5163ae4116cb4f2cee2e
Archit Khode <arkits@outlook.com>
2026-01-05 18:04:52 -0800

    fix: update base URL retrieval logic to use BASE_URL environment variable for deployment

--- src/client/TRPCProvider.tsx
+++ src/client/TRPCProvider.tsx
@@ -5,7 +5,7 @@ import { trpc } from "./trpc";
 
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.BASE_URL) return process.env.BASE_URL;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
